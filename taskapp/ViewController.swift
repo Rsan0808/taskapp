af9a5a31b018e533acd974533855358ad9322924
Rsan0808 <rsandane@gmail.com>
2019-10-21 22:28:43 +0900

    課題

--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -7,26 +7,65 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    //課題のとこ
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar){
+        let searchText = searchBar.text!
+        if(searchText != ""){
+            let predicate = NSPredicate(format: "category = %@", searchText)
+            taskArray = realm.objects(Task.self).filter(predicate)
+        } else {
+            taskArray = realm.objects(Task.self).sorted(byKeyPath: "date", ascending: false)
+        }
+        tableView.reloadData()
+    }
+    
+    //Realmインスタンスを取得する
+    let realm = try! Realm()
+    
+    
+    //DB内のタスクが管理されているリスト
+    //日付が近い順でソート：降順
+    //以降の内容をアップデートするとリスト内は自動で更新される
+    var taskArray = try! Realm().objects(Task.self).sorted(byKeyPath: "date", ascending: false)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
+    
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
 
-    // セルの数を返すメソッド
+    // データの数を返すメソッド
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return taskArray.count
     }
     
     // 各セルの内容を返すメソッド
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // 再利用可能な cell を得る
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString:String = formatter.string(from: task.date)
+        cell.detailTextLabel?.text = dateString
         
         return cell
     }
@@ -43,6 +82,32 @@ class ViewController: UIViewController {
     
     // Delete ボタンが押された時に呼ばれるメソッド
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            try! realm.write{
+                self.realm.delete(self.taskArray[indexPath.row])
+                tableView.deleteRows(at: [indexPath], with: .fade)
+            }
+        }
+    }
+    
+    //segueで画面遷移するときに呼ばれる
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?){
+        let inputViewController:InputViewController = segue.destination as! InputViewController
+        
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        } else {
+            let task = Task()
+            task.date = Date()
+            
+            let allTasks = realm.objects(Task.self)
+            if allTasks.count != 0 {
+                task.id = allTasks.max(ofProperty: "id")! + 1
+            }
+            
+            inputViewController.task = task
+        }
     }
 
 }
